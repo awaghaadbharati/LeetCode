1184499ec44dd9203aa20372e5b231465a81d293
Bharati Awaghad <awaghad.bharati@gmail.com>
2020-12-01 21:23:56 +0530

    December Leetcoding challenge

---
+++ MaximumDepthOfTree.java
@@ -0,0 +1,21 @@
+
+public class MaximumDepthOfTree {
+	public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        else
+            return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
